517bb5c0747c2b264c2250dbf0fb83f9caa6bf45
Lazir Pascual <lazirpascual@gmail.com>
2022-03-02 20:29:05 -0500

    add feature to change background image

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,12 @@ import SearchForm from "./Components/SearchForm/SearchForm";
 import NameBlock from "./Components/NameBlock/NameBlock";
 import CurrentWeatherBlock from "./Components/CurrentWeatherBlock/CurrentWeatherBlock";
 import SevenDayBlock from "./Components/SevenDayBlock/SevenDayBlock";
-import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
-import IconButton from "@mui/material/IconButton";
 import { CurrentWeather } from "./Interfaces/Interface";
 import Grid from "@mui/material/Grid";
-import "./App.css";
 import GoogleSearch from "./Components/GoogleSearch/GoogleSearch";
 import Header from "./Components/Header/Header";
+import BackgroundDialog from "./Components/BackgroundDialog/BackgroundDialog";
+import "./App.css";
 
 function App() {
   const [name, setName] = useState<string>("");
@@ -20,11 +19,13 @@ function App() {
   const [dailyWeatherList, setDailyWeatherList] = useState<CurrentWeather[]>(
     []
   );
+  const [background, setBackground] = useState<string>("");
 
   useEffect(() => {
     const fetchLocalData = async () => {
       const city = localStorage.getItem("city");
       const name = localStorage.getItem("name");
+      const background = localStorage.getItem("background");
 
       try {
         if (city && name) {
@@ -40,6 +41,10 @@ function App() {
           setDailyWeatherList(oneCallData.dailyData);
           setName(name);
         }
+
+        if (background) {
+          setBackground(background);
+        }
       } catch (error) {
         console.log(error);
       }
@@ -54,31 +59,38 @@ function App() {
     localStorage.setItem("name", "");
   };
 
+  const backgroundStyle = {
+    backgroundImage: `url(${background})`,
+  };
+
   return (
-    <div className="start-container">
-      {currentWeather === null ? (
-        <SearchForm
-          name={name}
-          setName={setName}
-          setCurrentWeather={setCurrentWeather}
-          setDailyWeatherList={setDailyWeatherList}
-        />
-      ) : (
-        <div>
-          <Header handleBackClick={handleBackClick} />
-          <Grid container className="weather-top">
-            <NameBlock name={name} />
-            <CurrentWeatherBlock currentWeather={currentWeather} />
-          </Grid>
-          <div className="weather-bottom">
-            <SevenDayBlock
-              dailyWeatherList={dailyWeatherList}
-              setCurrentWeather={setCurrentWeather}
-            />
+    <div className="start-container" style={backgroundStyle}>
+      <div>
+        {currentWeather === null ? (
+          <SearchForm
+            name={name}
+            setName={setName}
+            setCurrentWeather={setCurrentWeather}
+            setDailyWeatherList={setDailyWeatherList}
+          />
+        ) : (
+          <div>
+            <Header handleBackClick={handleBackClick} />
+            <Grid container className="weather-top">
+              <NameBlock name={name} />
+              <CurrentWeatherBlock currentWeather={currentWeather} />
+            </Grid>
+            <div className="weather-bottom">
+              <SevenDayBlock
+                dailyWeatherList={dailyWeatherList}
+                setCurrentWeather={setCurrentWeather}
+              />
+            </div>
+            <GoogleSearch />
           </div>
-          <GoogleSearch />
-        </div>
-      )}
+        )}
+      </div>
+      <BackgroundDialog setBackground={setBackground} />
     </div>
   );
 }
---
+++ src/Components/BackgroundDialog/BackgroundDialog.tsx
@@ -0,0 +1,93 @@
+import { useState, Dispatch, SetStateAction } from "react";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemText from "@mui/material/ListItemText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Dialog from "@mui/material/Dialog";
+import PersonIcon from "@mui/icons-material/Person";
+import { blue } from "@mui/material/colors";
+import "./BackgroundDialog.css";
+
+const backgroundList = [
+  {
+    description: "Standard",
+    image: "https://i.imgur.com/hj2L1tO.jpg",
+  },
+  {
+    description: "Night",
+    image: "https://i.imgur.com/Iwcvizh.jpg",
+  },
+  {
+    description: "Mountain Top",
+    image: "https://i.imgur.com/DeP2Jmd.jpg",
+  },
+  {
+    description: "Snow",
+    image: "https://i.imgur.com/XkqBWj8.jpg",
+  },
+  {
+    description: "Grass",
+    image: "https://i.imgur.com/9ZBogM3.jpg",
+  },
+  {
+    description: "Orange Clouds",
+    image: "https://i.imgur.com/RTwOGna.jpg",
+  },
+  {
+    description: "Blue Sky",
+    image: "https://i.imgur.com/tj73svH.jpg",
+  },
+];
+
+interface Props {
+  setBackground: Dispatch<SetStateAction<string>>;
+}
+
+const BackgroundDialog: React.FC<Props> = ({ setBackground }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleListItemClick = (image: string) => {
+    handleClose();
+    setBackground(image);
+    localStorage.setItem("background", image);
+  };
+
+  return (
+    <div className="BackgroundButton">
+      <br />
+      <Button variant="outlined" color="inherit" onClick={handleClickOpen}>
+        Background
+      </Button>
+      <Dialog onClose={handleClose} open={open}>
+        <DialogTitle>Set Background Image</DialogTitle>
+        <List sx={{ pt: 0 }}>
+          {backgroundList.map(({ description, image }) => (
+            <ListItem
+              button
+              onClick={() => handleListItemClick(image)}
+              key={description}
+            >
+              <ListItemAvatar>
+                <Avatar src={image} />
+              </ListItemAvatar>
+              <ListItemText primary={description} />
+            </ListItem>
+          ))}
+        </List>
+      </Dialog>
+    </div>
+  );
+};
+
+export default BackgroundDialog;
